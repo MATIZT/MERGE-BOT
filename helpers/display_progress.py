22d6cf25bd308db22bb001890ba7cecdae5e5241
MATIZT <147545725+MATIZT@users.noreply.github.com>
2023-12-05 10:22:03 +0530

    Update display_progress.py

--- helpers/display_progress.py
+++ helpers/display_progress.py
@@ -91,7 +91,7 @@ class Progress:
             # cpu = "{psutil.cpu_percent()}%"
             tmp = (
                 progress
-                + "\n**⌧ Total 🗃:**` 〚{1}〛`\n**⌧ Done ✅ :**` 〚{0}〛`\n**⌧ Speed 📊 :** ` 〚{2}/s〛`\n**⌧ ETA 🔃 :**` 〚{3}〛`\n {4}".format(
+                + "\n**🗃 Total :**` 〚{1}〛`\n**✅ Done  :**` 〚{0}〛`\n**⚡ Speed :** ` 〚{2}/s〛`\n**⏳ ETA   :**` 〚{3}〛`\n {4}".format(
                     humanbytes(current),
                     humanbytes(total),
                     humanbytes(speed),
